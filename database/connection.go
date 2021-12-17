9fe91fc1c46680b955bcdd3c61cda236a180c5ba
Sebastine Udeh <sommy@mail.com>
2021-12-17 16:41:58 +0100

    first commit

---
+++ database/connection.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"os"
+
+	"github.com/sommysab/React-Go/go-api/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	connection, err := gorm.Open(mysql.Open("root:"+os.Getenv("MYSQL_PASSWORD")+"@/testdb"), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to the database")
+	}
+
+	DB = connection
+
+	connection.AutoMigrate(&models.User{})
+}
